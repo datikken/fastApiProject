aadf8403a0658e7c8f6539e4d0bd1fd505e9a1b8
nshadybekov <shadybekov@cointelegraph.com>
2023-02-24 18:26:18 +0500

    jsonable_encoder added

--- app/database/models.py
+++ app/database/models.py
@@ -2,12 +2,31 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy import inspect
+import datetime
+import enum
+
+
+class Gender(enum.Enum):
+    M = 'M'
+    F = 'F'
 
 
 class Base(DeclarativeBase):
+    # TODO: decide maybe remove
     def _asdict(self):
         return {c.key: getattr(self, c.key)
                 for c in inspect(self).mapper.column_attrs}
+
+
+class Employee(Base):
+    __tablename__ = 'employees'
+
+    emp_no: Mapped[int] = mapped_column(primary_key=True)
+    birth_date: Mapped[datetime.datetime]
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    gender: Mapped[Gender]
+    hire_date: Mapped[datetime.datetime]
 
 
 class Department(Base):
--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from database.connection import engine
 from database.models import Base
 from database.models import Department
+from database.models import Employee
 from database.connection import async_session
 from sqlalchemy.sql import select
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
@@ -15,9 +17,17 @@ async def init_tables():
         await conn.run_sync(Base.metadata.create_all)
 
 
+@app.get("/employee/all")
+async def employees():
+    async with async_session.begin() as session:
+        data = await session.execute(select(Employee))
+        emps = [jsonable_encoder(row) for row in data.scalars()]
+    return emps
+
+
 @app.get("/department/all")
 async def root():
     async with async_session.begin() as session:
         data = await session.execute(select(Department))
-        depts = [row._asdict() for row in data.scalars()]
+        depts = [jsonable_encoder(row) for row in data.scalars()]
     return JSONResponse(content=depts)
